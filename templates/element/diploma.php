8776d4490508a8ca820b833b3a6bf9950d536566
Marat Dallin <mail@dallin.uz>
2024-07-26 09:45:05 +0500

    Fix diploma template.
     On branch master
     Your branch is up to date with 'origin/master'.
    
     Changes to be committed:
    	modified:   templates/element/diploma.php

--- templates/element/diploma.php
+++ templates/element/diploma.php
@@ -81,7 +81,7 @@
                 <table style="width: 100%; font-size: 24px; margin-bottom: 40mm;">
                     <tr>
                         <td style="width: 50%;">Rektor</td>
-                        <td style="text-align: right">DSc., A.V. Shin</td>
+                        <td style="text-align: right">DSc. A.V. Shin</td>
                     </tr>
                 </table>
 
